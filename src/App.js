37f39c001fa5fe99ba85912da649858a36f7f0c3
Kavita Jakhar <Kavya21@gmail.com>
2023-01-16 15:55:12 +0530

    creating navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Navbar from "./components/Navbar";
+
+
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-4xl my-2  mx-3 font-extrabold">hello</h1>
-    </div>
+    <>
+    <Navbar />
+    {/* <Home />
+    <About />
+    <Portfolio />
+    <Experience />
+    <Contact />
+
+    <SocialLinks /> */}
+    </>
   );
 }
 
